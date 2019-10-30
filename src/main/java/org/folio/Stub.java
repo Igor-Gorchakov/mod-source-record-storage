ebea7466e4477477a100a2631e6fe6e3666420a4
Igor-Gorchakov <Igor_Gorchakov@epam.com>
2019-10-30 21:24:39 +0200

    Added stub tests to check coverage results

---
+++ src/main/java/org/folio/Stub.java
@@ -0,0 +1,4 @@
+package org.folio;
+
+public class Stub {
+}
---
+++ src/test/java/org.folio/StubTest.java
@@ -0,0 +1,13 @@
+package org.folio;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class StubTest {
+
+  @Test
+  public void shouldCreateStub() {
+    Stub stub = new Stub();
+    Assert.assertNotNull(stub);
+  }
+}
